26073c4cc8e2cc8e74c5de6f8d43a296a73ce2d2
Indrek Juhkam <indrek@urgas.eu>
2010-11-24 18:24:22 +0200

    few formatting changes

--- lib/rlastfm/album.rb
+++ lib/rlastfm/album.rb
@@ -9,15 +9,15 @@ module RLastFM
       # you supply either the artist and track params or the mbid param.
       #
       # == Parameters
-      #  <tt>artist</tt> - The artist name.
-      #  <tt>album</tt> - The album name.
-      #  <tt>country</tt> - A country name, as defined by the ISO 3166-1
-      #     country names standard.
+      # * +artist+ - The artist name.
+      # * +album+ - The album name.
+      # * +country+ - A country name, as defined by the ISO 3166-1
+      #   country names standard.
       # == Optional parameters
-      #  <tt>mbid</tt> - The musicbrainz id for the album.
-      #  <tt>autocorrect</tt> - 1 or 0, transform misspelled artist names into
-      #     correct artist names, returning the correct version instead. The
-      #     corrected artist name will be returned in the response.
+      # * +mbid+ - The musicbrainz id for the album.
+      # * +autocorrect+ - 1 or 0, transform misspelled artist names into
+      #   correct artist names, returning the correct version instead. The
+      #   corrected artist name will be returned in the response.
       # == Example
       #  RLastFM::Album.get_buy_links("MGMT", "Congratulations", "United Kingdom")
       def get_buy_links(artist, album, country, options = {})
@@ -29,17 +29,17 @@ module RLastFM
       # artist name or a musicbrainz id.
       #
       # == Parameters
-      #  <tt>artist</tt> - The artist name.
-      #  <tt>album</tt> - The album name.
+      # * +artist+ - The artist name.
+      # * +album+ - The album name.
       # == Optional parameters
-      #  <tt>mbid</tt> - The musicbrainz id for the album.
-      #  <tt>lang</tt> - The language to return the biography in.
-      #  <tt>autocorrect</tt> - 1 or 0, transform misspelled artist names into
-      #     correct artist names, returning the correct version instead. The
-      #     corrected artist name will be returned in the response.
-      #  <tt>username</tt> - The username for the context of the request. If
-      #     supplied, the user's playcount for this album is included in the
-      #     response.
+      # * +mbid+ - The musicbrainz id for the album.
+      # * +lang+ - The language to return the biography in.
+      # * +autocorrect+ - 1 or 0, transform misspelled artist names into
+      #   correct artist names, returning the correct version instead. The
+      #   corrected artist name will be returned in the response.
+      # * +username+ - The username for the context of the request. If
+      #   supplied, the user's playcount for this album is included in the
+      #   response.
       # == Example
       #  RLastFM::Album.get_info("MGMT", "Congratulations")
       def get_info(artist, album, options = {})
@@ -50,14 +50,14 @@ module RLastFM
       # Get shouts for this album. Also available as an rss feed.
       #
       # == Parameters
-      #  <tt>artist</tt> - The artist name.
-      #  <tt>album</tt> - The album name.
+      # * +artist+ - The artist name.
+      # * +album+ - The album name.
       # == Optional parameters
-      #  <tt>mbid</tt> - The musicbrainz id for the album.
-      #  <tt>autocorrect</tt> - 1 or 0, transform misspelled artist names into
-      #     correct artist names, returning the correct version instead. The
-      #     corrected artist name will be returned in the response.
-      #  <tt>page</tt> - The page number to fetch.
+      # * +mbid+ - The musicbrainz id for the album.
+      # * +autocorrect+ - 1 or 0, transform misspelled artist names into
+      #   correct artist names, returning the correct version instead. The
+      #   corrected artist name will be returned in the response.
+      # * +page+ - The page number to fetch.
       # == Example
       #  RLastFM::Album.get_shouts("MGMT", "Congratulations")
       def get_shouts(artist, album, options = {})
@@ -68,13 +68,13 @@ module RLastFM
       # Get the tags applied by an individual user to an album on Last.fm.
       #
       # == Parameters
-      #  <tt>artist</tt> - The artist name.
-      #  <tt>album</tt> - The album name.
+      # * +artist+ - The artist name.
+      # * +album+ - The album name.
       # == Optional parameters
-      #  <tt>mbid</tt> - The musicbrainz id for the album.
-      #  <tt>autocorrect</tt> - 1 or 0, transform misspelled artist names into
-      #     correct artist names, returning the correct version instead. The
-      #     corrected artist name will be returned in the response.
+      # * +mbid+ - The musicbrainz id for the album.
+      # * +autocorrect+ - 1 or 0, transform misspelled artist names into
+      #   correct artist names, returning the correct version instead. The
+      #   corrected artist name will be returned in the response.
       # == Example
       #  RLastFM::Album.get_tags("MGMT", "Congratulations")
       def get_tags(artist, album, options = {})
@@ -84,13 +84,13 @@ module RLastFM
       # Get the top tags for an album on Last.fm, ordered by popularity.
       #
       # == Parameters
-      #  <tt>artist</tt> - The artist name.
-      #  <tt>album</tt> - The album name.
+      # * +artist+ - The artist name.
+      # * +album+ - The album name.
       # == Optional parameters
-      #  <tt>mbid</tt> - The musicbrainz id for the album.
-      #  <tt>autocorrect</tt> - 1 or 0, transform misspelled artist names into
-      #     correct artist names, returning the correct version instead. The
-      #     corrected artist name will be returned in the response.
+      # * +mbid+ - The musicbrainz id for the album.
+      # * +autocorrect+ - 1 or 0, transform misspelled artist names into
+      #   correct artist names, returning the correct version instead. The
+      #   corrected artist name will be returned in the response.
       # == Example
       #  RLastFM::Album.get_top_tags("MGMT", "Congratulations")
       def get_top_tags(artist, album, options = {})
@@ -105,12 +105,12 @@ module RLastFM
       # Search for an album by name. Returns album matches sorted by relevance.
       #
       # == Parameters
-      #  <tt>album</tt> - The album name.
+      # * +album+ - The album name.
       # == Optional parameters
-      #  <tt>limit</tt> - Limit the number of albums at one time. Default
-      #     (maximum) is 30.
-      #  <tt>page</tt> - Scan into the results by specifying a page number.
-      #     Defaults to first page.
+      # * +limit+ - Limit the number of albums at one time. Default
+      #   (maximum) is 30.
+      # * +page+ - Scan into the results by specifying a page number.
+      #   Defaults to first page.
       # == Example
       #  RLastFM::Album.search("Congratulations")
       def search(album, options = {})
